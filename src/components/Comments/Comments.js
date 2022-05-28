70fc5828e6b50f1a01cee5204cfcec784c46d87d
Michael Nguyen <n93michael@gmail.com>
2022-05-28 09:41:02 -0400

    Mobile styling done for comments section. Minor BEM changes done. Minor spacing changes throughout mobile design

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,37 +5,36 @@ function Comments({ selectedVideo, convertTimeToDate }) {
   const comments = selectedVideo.comments;
 
   return (
-    <div className="comment">
-      <div className="comment__container">
-        <h4 className="comment__count">3 Comments</h4>
-
-        <div className="form">
-          <img className="avatar" src={avatar} alt="Avatar Image" />
-
-          <form className="new-comment__form">
-            <label className="new-comment__label">
-              JOIN THE CONVERSATION
-              <textarea
-                placeholder="Add a comment"
-                className="new-comment__input"
-              ></textarea>
-            </label>
-            <button className="new-comment__button">COMMENT</button>
-          </form>
+    <div className="comments">
+      <h4 className="comments__count">3 Comments</h4>
+      <div className="form">
+        <div className="form__icon">
+          <img className="avatar" src={avatar} alt="Avatar" />
         </div>
+
+        <form className="form__container">
+          <label className="form__name">
+            JOIN THE CONVERSATION
+            <textarea
+              placeholder="Add a comment"
+              className="form__input"
+            ></textarea>
+          </label>
+          <button className="button button--form">COMMENT</button>
+        </form>
       </div>
 
-      <div className="comments">
+      <div className="comment">
         {comments.map((comment, index) => {
           return (
-            <div className="comments__card" key={index}>
-              <div className="comments__icon"></div>
-              <div className="comments__container">
-                <p className="comments__name">{comment.name}</p>
-                <p className="comments__date">
+            <div className="comment__card" key={index}>
+              <div className="comment__icon"></div>
+              <div className="comment__container">
+                <p className="comment__name">{comment.name}</p>
+                <p className="comment__date">
                   {convertTimeToDate(new Date(comment.timestamp))}
                 </p>
-                <p className="comments__entry">{comment.comment}</p>
+                <p className="comment__entry">{comment.comment}</p>
               </div>
             </div>
           );
